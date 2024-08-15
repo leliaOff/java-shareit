88efe8adadb17516724a328edef6cdc4f2d52362
Elena Polukova <e.brykova@yofin.ru>
2024-08-15 10:25:19 +0400

    add: функциональность "Пользователи"

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.user;
 import lombok.Data;
 
 @Data
-public class User {
+public class UserDto {
     private Long id;
     private String email;
     private String name;
---
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -0,0 +1,65 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.base.BaseRepository;
+import ru.practicum.shareit.base.exception.InternalServerException;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Repository
+public class UserRepository extends BaseRepository<User> {
+
+    public UserRepository(JdbcTemplate jdbc, RowMapper<User> mapper) {
+        super(jdbc, mapper);
+    }
+
+    public Collection<User> get() {
+        return get("SELECT * FROM users");
+    }
+
+    public Optional<User> find(Long id) {
+        return find("SELECT * FROM users WHERE id = ?", id);
+    }
+
+    public Optional<User> find(String email) {
+        return find("SELECT * FROM users WHERE email = ?", email);
+    }
+
+    public Optional<User> create(User user) {
+        try {
+            Long id = create(
+                    "INSERT INTO users(email, name) VALUES (?, ?)",
+                    user.getEmail(),
+                    user.getName()
+            );
+            user.setId(id);
+        } catch (InternalServerException exception) {
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+
+    public Optional<User> update(User user) {
+        try {
+            update(
+                    "UPDATE users SET email = ?, name = ? WHERE id = ?",
+                    user.getEmail(),
+                    user.getName(),
+                    user.getId()
+            );
+        } catch (InternalServerException exception) {
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+
+    public void delete(User user) {
+        delete(
+                "DELETE FROM users WHERE id = ?",
+                user.getId()
+        );
+    }
+}
